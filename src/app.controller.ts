c0ddcacb76511d6bd6782894de6f6bb6cc6c54e6
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-09-22 00:01:28 +0300

    Добавлен webhook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { exec } from 'child_process';
 
 @Controller()
 export class AppController {
@@ -9,5 +17,23 @@ export class AppController {
   getHello(): string {
     console.log('test123');
     return this.appService.getHello();
+  }
+
+  @Post('webhook')
+  @HttpCode(HttpStatus.OK)
+  handleWebhook(@Body() body: any): void {
+    console.log('Received webhook:', body);
+
+    exec('docker restart job-search-service_app', (error, stdout, stderr) => {
+      if (error) {
+        console.error(`Error restarting container: ${error.message}`);
+        return;
+      }
+      if (stderr) {
+        console.error(`Error output: ${stderr}`);
+        return;
+      }
+      console.log(`Container restarted: ${stdout}`);
+    });
   }
 }
