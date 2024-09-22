2091268a56c7a31cfef21fd83cb09b95bd8a15f4
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-09-22 14:27:52 +0300

    Обновлён тестовый метод для get запроса

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World and Hello everyone and How are youoooo!!! Test';
+    return 'Hello World and Hello everyone and How are youoooo!!! Test!!!!';
   }
 }
