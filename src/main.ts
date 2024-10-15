1c81cb2fa3c52866811e4f3fb21396f106dd7a48
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-10-15 12:59:40 +0300

    добавлен консоль лог для теста

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.APP_PORT || 8080;
   await app.listen(port);
-  console.log(process.env.NODE_ENV);
+  console.log('Test of app');
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 
