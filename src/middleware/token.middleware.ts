b5fc021161ee152a83ff2c78833b973ac750649d
FydorZbinyakov <zbinyakov@udpauto.ru>
2024-11-17 19:23:50 +0300

    fix cookie

--- src/middleware/token.middleware.ts
+++ src/middleware/token.middleware.ts
@@ -19,7 +19,6 @@ export class TokenMiddleware implements NestMiddleware {
       res.cookie('refreshToken', refreshToken, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: 'none',
         maxAge: 60 * 24 * 60 * 60 * 1000,
       });
     };
@@ -28,7 +27,6 @@ export class TokenMiddleware implements NestMiddleware {
       res.cookie('accessToken', accessToken, {
         httpOnly: true,
         secure: isProduction,
-        sameSite: 'none',
         maxAge: 15 * 60 * 1000,
       });
     };
